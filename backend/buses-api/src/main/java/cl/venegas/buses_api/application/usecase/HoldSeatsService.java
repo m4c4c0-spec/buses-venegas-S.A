cba2276fb2d276b105c23a0761a4ae7bde71b144
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-19 00:27:30 -0300

    Implementa logica de negocio en la capa de aplicacion
    Primero se movio la logica de negocio para definir el tiempo de expiracion arreglando el metodo cleanupExpiredHolds a este servicio ademas junto a esto ahora el servicio calcula el experiresAt y lo traspasa a repository, en lugar de que este decida la duracion y por ultimo se creo la carpeta task en el modulo de aplication por consiguiente en esta se crea el archivo ExpiredHoldsCleanupTask.java y en este nuevo archivo se crea una tarea programada @Scheduled que se ejecuta cada minuto por consecuencia esta es responsable de eliminar todos los seathold(reservas de asientos) que hayan expirado o liberando asi los asientos para que otros usuarios puedan reservarlos.

--- backend/buses-api/src/main/java/cl/venegas/buses_api/application/usecase/HoldSeatsService.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/application/usecase/HoldSeatsService.java
@@ -5,11 +5,21 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+
 @Service
 public class HoldSeatsService {
   private final SeatHoldRepository holds;
-  public HoldSeatsService(SeatHoldRepository holds){ this.holds = holds; }
+  private final long HOLD_DURATION_MINUTES = 10; 
+
+  public HoldSeatsService(SeatHoldRepository holds){ 
+    this.holds = holds; 
+  }
+
   public LocalDateTime handle(Long tripId, String seat, Long userId){
-    return holds.hold(tripId, seat, userId);
+    // La lógica de negocio vive aquí, en la capa de aplicación
+    var expiresAt = LocalDateTime.now().plusMinutes(HOLD_DURATION_MINUTES);
+    
+    // Pasamos la fecha de expiración al repositorio
+    return holds.hold(tripId, seat, userId, expiresAt);
   }
 }
