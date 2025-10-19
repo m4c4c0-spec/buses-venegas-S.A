ed6190b28dd31eb1cdcad9c43fed723203cb487e
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-19 02:00:26 -0300

    Creacion de Carpeta exception y el archivo RestExceptionHandler ademas de actualizarse los dos archivos HoldsController y HoldRequest en la capa de presentation
    Se creo un endpoint ControllerAdvice global para manejar excepciones ademas se implementa @ExceptionHandler que capturara la excepcion SeatAlreadyHeldException y devuelve una respueta 409 CONFLICT limpia al cliente junto a esto se modifica el archivo HoldsController eliminando el try-catch el controlador ahora es mucho mas limpio y la logica de manejo de errores que tenia se delego al RestExceptionHandler y por ultimo se modifico el HoldRequest.java logrando asi mejorar la seguridad al eliminar el campo UserID del request junto a eso el cliente ya no puede suplantar al usuario manipulando el json mediante una consulta en DOM del navegador ahora debe obtenerse del contexto de seguridad (autenticacion)

--- backend/buses-api/src/main/java/cl/venegas/buses_api/presentation/http/request/HoldRequest.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/presentation/http/request/HoldRequest.java
@@ -2,6 +2,5 @@ package cl.venegas.buses_api.presentation.http.request;
 
 import jakarta.validation.constraints.NotNull;
 
-public record HoldRequest(@NotNull Long tripId, @NotNull String seat, Long userId) {
-    
+public record HoldRequest(@NotNull Long tripId, @NotNull String seat) {    
 }
