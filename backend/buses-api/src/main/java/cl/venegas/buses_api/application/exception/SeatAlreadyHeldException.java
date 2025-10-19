610b192d82b6305ba6359d7a1185ced0757a9fa2
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-19 00:27:30 -0300

    Implementa logica de negocio en la capa de aplicacion
    Primero se movio la logica de negocio para definir el tiempo de expiracion arreglando el metodo cleanupExpiredHolds a este servicio ademas junto a esto ahora el servicio calcula el experiresAt y lo traspasa a repository, en lugar de que este decida la duracion y por ultimo se creo la carpeta task en el modulo de aplication por consiguiente en esta se crea el archivo ExpiredHoldsCleanupTask.java y en este nuevo archivo se crea una tarea programada @Scheduled que se ejecuta cada minuto por consecuencia esta es responsable de eliminar todos los seathold(reservas de asientos) que hayan expirado o liberando asi los asientos para que otros usuarios puedan reservarlos.

---
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/application/exception/SeatAlreadyHeldException.java
@@ -0,0 +1,7 @@
+package main.java.cl.venegas.buses_api.application.exception;
+
+public class SeatAlreadyHeldException extends RuntimeException {
+    public SeatAlreadyHeldException(String message) {
+        super(message);
+    }
+}
