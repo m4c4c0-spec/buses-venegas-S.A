652cc2af4d78c813822241bd9992bb349a4d4d5d
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-19 02:02:15 -0300

    Se modifica el archivo de ejecucion BusesApplication
    Se añadió la anotación @EnableScheduling para habilitar la ejecución de  nuevas tareas en ExpiredHoldsCleanupTask

--- backend/buses-api/src/main/java/cl/venegas/buses_api/BusesApiApplication.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/BusesApiApplication.java
@@ -2,8 +2,10 @@ package cl.venegas.buses_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BusesApiApplication {
 
 	public static void main(String[] args) {
