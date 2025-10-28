d3e80ec063dc72612ed2e096dbf69fd59cddd3aa
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-28 20:07:34 -0300

    Creacion de Route.java domain-model
    la creacion de este archivo tiene como finalidad definir los parametros del viaje, desde el punto de origen y el destino ademas de cuanto va a durar y precio del viaje como tal

---
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Route.java
@@ -0,0 +1,10 @@
+package main.java.cl.venegas.buses_api.domain.model;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+
+public record Route(Long id, String origin, String destination, Duration estimatedDuration, BigDecimal basePrice, Boolean active
+) {
+
+}
