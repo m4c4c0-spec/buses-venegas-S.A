ef604d42d71820c1fb98d5dde234595ea680d537
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-28 18:58:00 -0300

    Revert "Creacion de Passenger.java en domain-model"
    This reverts commit a0646c728cadb01317eeb14fd3fde69eeef5d016.

--- backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Passenger.java
+++
@@ -1,5 +0,0 @@
-package main.java.cl.venegas.buses_api.domain.model;
-
-public record Passenger(Long id, String firstName, String lastName, String documentType, String documentNumber, String email, String phone) {
-    
-}
