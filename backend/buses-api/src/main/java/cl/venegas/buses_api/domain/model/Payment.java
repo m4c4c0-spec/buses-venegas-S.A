95e769acb4511d890b77a27dd391a58313aecaa5
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-10-28 19:58:45 -0300

    Creacion archivo Payment.java
    la creacion de este archivo tiene como finalidad definir los datos de la estructura de pago utilizando un record para registrar transacciones de pago para el pago de los viajes ademas de rastrear el estado de pagos en el sistema junto con el almacenamiento de informacion de pagos externos y asi lograr ,mantener el historia de cada pago realizado

---
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Payment.java
@@ -0,0 +1,12 @@
+package main.java.cl.venegas.buses_api.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record Payment(Long id,Long bookingId, BigDecimal amount, PaymentMethod method, PaymentStatus status,
+    String transactionId,
+    String gatewayResponse,
+    LocalDateTime createdAt
+) {
+
+}
