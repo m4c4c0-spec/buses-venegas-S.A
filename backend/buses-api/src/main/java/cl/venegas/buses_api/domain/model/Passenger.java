b010eff57e07e23b67be811447d9b341d6f631e7
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-11-03 19:12:12 -0300

    actualizacion de Passenger.java en domain-model
    la finalidad de la actualizacion de este archivo es para correjir la ruta incorrecta de package, ya que estaba definida en el formato incorrecto

--- backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Passenger.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Passenger.java
@@ -1,4 +1,4 @@
-package main.java.cl.venegas.buses_api.domain.model;
+package cl.venegas.buses_api.domain.model;
 
 public record Passenger(Long id, String firstName, String lastName, String documentType, String documentNumber, String email, String phone) {
     
