0df6bec65863697af863ff7619636235a7f248b8
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-11-03 02:11:35 -0300

    actualizacion del archivo bus.java em domain-model
    la finalidad de la actualizacion de este archivo fue modificar el package que estaba mal definido asi eliminando el prefijo de main.java, definiendo correctamente la sintaxis del package ademas de arreglar la identacion de public record acomodando los archivos en formato vertical.

--- backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Bus.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Bus.java
@@ -1,7 +1,15 @@
-package main.java.cl.venegas.buses_api.domain.model;
+package cl.venegas.buses_api.domain.model;
 
 import java.time.LocalDate;
 
-public record Bus(Long id, String plateNumber, String model, Integer totalSeats, BusType type, 
-                    BusStatus status, LocalDate ultimaMantencion) {
+
+public record Bus(
+    Long id, 
+    String plateNumber, 
+    String model, 
+    Integer totalSeats, 
+    BusType type, 
+    BusStatus status, 
+    LocalDate ultimaMantencion
+) {
 }
