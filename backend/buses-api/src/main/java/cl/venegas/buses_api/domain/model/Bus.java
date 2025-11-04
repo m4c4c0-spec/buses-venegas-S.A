50e740032f75b6cad5ee0d46d6a341f287b478b2
JorgePfeifer <j.pfeifer01@ufromail.cl>
2025-11-04 20:32:48 -0300

    Correccion errroes Backend
    Se añade las clases BusStatus y UserRepository, ademas se solucion la importacion de Bus.java

--- backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Bus.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/model/Bus.java
@@ -3,5 +3,5 @@ package cl.venegas.buses_api.domain.model;
 import java.time.LocalDate;
 
 public record Bus(Long id, String plateNumber, String model, Integer totalSeats, cl.venegas.buses_api.domain.model.BusType type,
-                    BusStatus status, LocalDate ultimaMantencion) {
+                  cl.venegas.buses_api.domain.model.BusStatus status, LocalDate ultimaMantencion) {
 }
