03090aeac7dadd92e03228564d2fbf7c5b7e49d2
JorgePfeifer <j.pfeifer01@ufromail.cl>
2025-11-04 20:36:24 -0300

    Correcion SecurityConfing.java
    Se corrije SecurityConfig,java con una version mejorada

--- backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/security/SecurityConfig.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/security/SecurityConfig.java
@@ -1,31 +1,24 @@
 package cl.venegas.buses_api.infrastructure.security;
 
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import cl.venegas.buses_api.presentation.http.request.HoldRequest;
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 public class SecurityConfig {
-  @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-    return http.build();
-  }
-  @PostMapping("/holds")
-public ResponseEntity<?> hold(
-  @Validated @RequestBody HoldRequest req, 
-  @AuthenticationPrincipal UserDetails userDetails) {
-        Long userId = 1L; 
-        var expires = service.handle(req.tripId(), req.seat(), userId);
-    return ResponseEntity.ok(Map.of("expiresAt", expires.toString()));
-}
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
