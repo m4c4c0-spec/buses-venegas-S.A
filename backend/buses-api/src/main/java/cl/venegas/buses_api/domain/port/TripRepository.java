03320b6d3d479016706f40591dc90beedb60eb57
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-11-04 21:44:26 -0300

    Solucion de archivos conflictivos en la rama feature/backend-application
    la finalidad de este commit fue actualizar el gradle con los plugins faltantes y ademas agregar archivos ausentes como bus status  y mejorar la estructura de algunos archivos

--- backend/buses-api/src/main/java/cl/venegas/buses_api/domain/port/TripRepository.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/domain/port/TripRepository.java
@@ -3,7 +3,10 @@ package cl.venegas.buses_api.domain.port;
 import cl.venegas.buses_api.domain.model.Trip;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface TripRepository {
   List<Trip> findBy(String origin, String dest, LocalDate date);
+
+  Optional<Trip> findById(Long id);
 }
