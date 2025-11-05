c6fe206696f19359a4a641eac1607b9efdbea061
JorgePfeifer <j.pfeifer01@ufromail.cl>
2025-11-05 01:00:50 -0300

    Solucion merge
    se solucionan los conflictos provocados luego del merge

--- backend/buses-api/src/main/java/cl/venegas/buses_api/presentation/http/controller/SeatHoldController.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/presentation/http/controller/SeatHoldController.java
@@ -9,33 +9,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import cl.venegas.buses_api.presentation.http.request.HoldRequest;
 
-// import cl.venegas.buses_api.application.service.SeatHoldService;
-
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class SeatHoldController {
-
-
-    // private final SeatHoldService service;
-
-    // public SeatHoldController(SeatHoldService service) {
-    //     this.service = service;
-    // }
 
     @PostMapping("/holds")
     public ResponseEntity<?> hold(
             @Validated @RequestBody HoldRequest req,
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        Long userId = 1L;
-
-        // var expires = service.handle(req.tripId(), req.seat(), userId);
-        // return ResponseEntity.ok(Map.of("expiresAt", expires.toString()));
-
-
+        // Endpoint en construcción
         return ResponseEntity.ok(Map.of("message", "Endpoint en construcción"));
     }
 }
