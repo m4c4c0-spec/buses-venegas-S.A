d5f38b4912726f314450f17b65cba83682af1303
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-11-04 22:57:58 -0300

    Solucion de archivos conflictivos en backend-application
    la finalidad de la actualizacion de estos archivos fue para solucionar la corrupcion de algunos y agregarl algunos metodos ausentes

--- backend/buses-api/src/main/java/cl/venegas/buses_api/application/usecase/HoldSeatsService.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/application/usecase/HoldSeatsService.java
@@ -1,29 +1,23 @@
 package cl.venegas.buses_api.application.usecase;
 
-import cl.venegas.buses_api.domain.model.SeatHold;
-import cl.venegas.buses_api.domain.port.SeatHoldRepository;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Service;
+
+import cl.venegas.buses_api.domain.model.SeatHold;
+import cl.venegas.buses_api.domain.port.SeatHoldRepository;
 
 @Service
 public class HoldSeatsService {
   private final SeatHoldRepository holds;
   private final long HOLD_DURATION_MINUTES = 10; 
 
-  public HoldSeatsService(SeatHoldRepository holds){ 
+  public HoldSeatsService(SeatHoldRepository holds) { 
     this.holds = holds; 
   }
 
-  public LocalDateTime handle(Long tripId, String seat, Long userId){
-
+  public SeatHold handle(Long tripId, String seat, Long userId) {
     var expiresAt = LocalDateTime.now().plusMinutes(HOLD_DURATION_MINUTES);
-
     return holds.hold(tripId, seat, userId, expiresAt);
   }
-
-
-
-
 }
--- backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/persistence/jpa/repo/SeatHoldJpaRepository.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/persistence/jpa/repo/SeatHoldJpaRepository.java
@@ -1,10 +1,15 @@
 package cl.venegas.buses_api.infrastructure.persistence.jpa.repo;
 
+import cl.venegas.buses_api.domain.model.SeatHold;
 import cl.venegas.buses_api.infrastructure.persistence.jpa.entity.SeatHoldJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 
 public interface SeatHoldJpaRepository extends JpaRepository<SeatHoldJpa, Long> {   
     void deleteByExpiresAtBefore(LocalDateTime now);
+
+    Collection<SeatHold> findByTripIdAndSeatIn(Long tripId, List<String> seatNumbers);
 }
