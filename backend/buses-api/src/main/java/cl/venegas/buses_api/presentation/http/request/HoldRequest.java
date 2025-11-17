3634171fe4d3cddde4c895ebc708c4bb2c233b0a
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2025-11-17 00:26:31 -0300

    Eliminacion de la capa Presentation -  Para la migracion a Onion
    La finalidad de la eliminacion de esta capa es para eliminar la capa extra que representaba una capa extra de seguridad para proteger el demonio de las amenazas  externas ya que parte desde el exterior hacia el interior que en este caso seria domain y para darle paso a la capa interfaces que se encargara del manejo de los datos de la api.

--- backend/buses-api/src/main/java/cl/venegas/buses_api/presentation/http/request/HoldRequest.java
+++
@@ -1,6 +0,0 @@
-package cl.venegas.buses_api.presentation.http.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record HoldRequest(@NotNull Long tripId, @NotNull String seat) {    
-}
