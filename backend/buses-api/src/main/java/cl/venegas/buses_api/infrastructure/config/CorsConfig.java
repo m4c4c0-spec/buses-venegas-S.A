ba8b26a438226c263fc0801a4840a34282468b71
Marco-Venegas-Poveda <m.venegas14@ufromail.cl>
2026-03-30 20:09:50 -0300

    fix: allow all origin patterns for external universal testing

--- backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/config/CorsConfig.java
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/infrastructure/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins(allowedOrigins.split(","))
+        .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowCredentials(true);
   }
